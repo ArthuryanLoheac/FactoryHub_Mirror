e1a3f2392c4ffcd7e4657fc55055a573da67cdc1
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-02-20 17:35:13 +0100

    🚧 Cmake

---
+++ myBuild.sh
@@ -0,0 +1,3 @@
+cmake . -B ./build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+
+cmake --build build
--- src/items/amunition.cpp
+++ src/items/amunition.cpp
@@ -1,3 +1,10 @@
+/*
+** EPITECH PROJECT, 2025
+** FactoryHub
+** File description:
+** amunition
+*/
+
 #include "amunition.hpp"
 
 size_t AAmunition::getTier() const
