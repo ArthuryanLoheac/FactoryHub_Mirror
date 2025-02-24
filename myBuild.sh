bcbcfab80a5b227143c88e68c26bd40f6875b502
JeanBizeul <jean.bizeul@epitech.eu>
2025-02-24 15:25:39 +0100

    🔨 Add the build of glfw to the cmakefiles

--- myBuild.sh
+++ myBuild.sh
@@ -1,3 +1,6 @@
-cmake . -B ./build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+git submodule init
+git submodule update
+
+cmake -DGLFW_BUILD_DOCS=OFF -S . -B ./build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
 
 cmake --build build
