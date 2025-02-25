d82d04dae5a8197aeeac4ac73a452c1fa66dfd8f
Arthuryan Lohéac <146721013+ArthuryanLoheac@users.noreply.github.com>
2025-02-25 15:42:38 +0100

    ✨ Add Base Class

---
+++ src/Game/Blocks/Builds/Base.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** FactoryHub
+** File description:
+** Base
+*/
+
+#pragma once
+#include "ABuilds.hpp"
+
+class Base : public ABuilds
+{
+    public:
+        Base();
+};
