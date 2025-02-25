9c45d68c9c2a78ced1009fd916e66cb985a2fbfe
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-02-25 17:56:23 +0100

    🚧 WIP generation

---
+++ src/Game/Map/Generation.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2025
+** FactoryHub
+** File description:
+** Generation
+*/
+#include "MapGrid.hpp"
+#include "Zinc.hpp"
+
+void generateAll(MapGrid &map)
+{
+    map = map;
+}
--- src/Game/Map/MapGrid.cpp
+++ src/Game/Map/MapGrid.cpp
@@ -9,14 +9,9 @@
 
 MapGrid::MapGrid(size_t X, size_t Y)
 {
-    for (size_t i = 0; i < X; i++) {
-        std::vector<std::vector<std::shared_ptr<IBlock>>> tmp;
-        for (size_t j = 0; j < Y; j++) {
-            std::vector<std::shared_ptr<IBlock>> tmp2;
-            tmp.push_back(tmp2);
-        }
-        _grid.push_back(tmp);
-    }
+    initEmptyMap(X, Y);
+    _sizeX = X;
+    _sizeY = Y;
 }
 
 std::shared_ptr<IBlock> MapGrid::GetIBlockAtPos(size_t X, size_t Y, size_t Z)
@@ -45,4 +40,27 @@ void MapGrid::deleteBlock(size_t X, size_t Y, size_t Z)
     if (X >= _sizeX || Y >= _sizeY || Z >= _grid[X][Y].size())
         throw std::out_of_range("Out of range");
     _grid[X][Y].erase(_grid[X][Y].begin() + Z);
+}
+
+void MapGrid::initEmptyMap(size_t X, size_t Y)
+{
+    for (size_t i = 0; i < X; i++) {
+        std::vector<std::vector<std::shared_ptr<IBlock>>> tmp;
+        for (size_t j = 0; j < Y; j++) {
+            std::vector<std::shared_ptr<IBlock>> tmp2;
+            tmp.push_back(tmp2);
+        }
+        _grid.push_back(tmp);
+    }
+}
+
+std::ostream &operator<<(std::ostream &os, MapGrid &mapGrid)
+{
+    for (size_t x = 0; x < mapGrid.getSizeX(); x++) {
+        for (size_t y = 0; y < mapGrid.getSizeY(); y++) {
+            os << mapGrid.getAllBlocksAtPos(x, y).size() << " ";
+        }
+        os << std::endl;
+    }
+    return os;
 }
