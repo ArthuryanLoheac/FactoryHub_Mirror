56565cee3eed58478684557b6a3504c308a56c7d
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-03-11 10:33:23 +0100

    ✨ Add Receipe High_Power_EMP_Charge

--- src/Game/Blocks/Builds/Factory/Arthuryan_Module.cpp
+++ src/Game/Blocks/Builds/Factory/Arthuryan_Module.cpp
@@ -12,6 +12,7 @@ Arthuryan_Module::Arthuryan_Module() : AFactory()
     _sprite = new sdf::Sprite(glm::vec3(0.0f, 0.0f, 0.0f),
         sdf::GetterTextures::instance->getTexture("Arthuryan_Module"), 0.0f);
 
+    // Recette Condensed_Plasma
     std::vector<Item> r_in1;
     r_in1.push_back(Item("Gunpowder"));
     r_in1.push_back(Item("WaterRaw"));
@@ -21,4 +22,15 @@ Arthuryan_Module::Arthuryan_Module() : AFactory()
     Recette r_Condensed_Plasma(r_in1, r_out1, 15.f);
 
     _Recettes.push_back(r_Condensed_Plasma);
+
+    // Recette High_Power_EMP_Charge
+    std::vector<Item> r_in2;
+    r_in2.push_back(Item("Energy_Capacitor"));
+    r_in2.push_back(Item("WaterRaw"));
+    r_in2.push_back(Item("Reinforced_Brass"));
+    std::vector<Item> r_out2;
+    r_out2.push_back(Item("High_Power_EMP_Charge"));
+    Recette r_High_Power_EMP_Charge(r_in2, r_out2, 15.f);
+
+    _Recettes.push_back(r_High_Power_EMP_Charge);
 }
