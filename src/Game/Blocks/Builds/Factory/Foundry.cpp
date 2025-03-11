73552b2c00aae84b36a3d218008f47d7779169f8
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-03-11 10:03:00 +0100

    ✨ Add Receipe Reinforced_Socket

--- src/Game/Blocks/Builds/Factory/Foundry.cpp
+++ src/Game/Blocks/Builds/Factory/Foundry.cpp
@@ -8,9 +8,10 @@
 
 FoundryT1::FoundryT1() : AFactory()
 {
-    sdf::Texture textureFoundry = sdf::Texture("Assets/Foundry.png");
-    _sprite = new sdf::Sprite(glm::vec3(0.0f, 0.0f, 0.0f), textureFoundry);
+    _sprite = new sdf::Sprite(glm::vec3(0.0f, 0.0f, 0.0f),
+        sdf::GetterTextures::instance->getTexture("Foundry"), 0.0f);
 
+    // Recette steel_bar
     std::vector<Item> r_in1;
     r_in1.push_back(Item("SteelRaw"));
     std::vector<Item> r_out1;
@@ -18,4 +19,14 @@ FoundryT1::FoundryT1() : AFactory()
     Recette r_steel_bar(r_in1, r_out1, 2.f);
 
     _Recettes.push_back(r_steel_bar);
+
+    // Recette Reinforced_Socket
+    std::vector<Item> r_in2;
+    r_in2.push_back(Item("Reinforced_Brass"));
+    r_in2.push_back(Item("SteelRaw"));
+    std::vector<Item> r_out2;
+    r_out2.push_back(Item("Reinforced_Socket"));
+    Recette r_Reinforced_Socket(r_in2, r_out2, 2.f);
+
+    _Recettes.push_back(r_Reinforced_Socket);
 }
