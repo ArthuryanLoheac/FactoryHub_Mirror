67e48ab5b1c29889488a8c7e916a44cbbd3174ce
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-03-11 10:05:21 +0100

    ✨ Add Receipe Energy_Capacitor

---
+++ src/Game/Blocks/Builds/Factory/Capacitor_Factory.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** FactoryHub
+** File description:
+** Capacitor_Factory
+*/
+
+#pragma once
+#include "AFactory.hpp"
+
+class Capacitor_Factory : public AFactory
+{
+    public:
+        Capacitor_Factory();
+};
