5d2b97c77c40c527c921ac4b15ec12ddc5fcbcce
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-03-11 10:41:37 +0100

    ✨ Add Receipe Steel_Darts

---
+++ src/Game/Blocks/Builds/Factory/Cutting_Machine.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** FactoryHub
+** File description:
+** Smelter
+*/
+
+#pragma once
+#include "AFactory.hpp"
+
+class Cutting_Machine : public AFactory
+{
+    public:
+        Cutting_Machine();
+};
