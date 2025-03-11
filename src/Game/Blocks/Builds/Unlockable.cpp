08949cc6a4f27331238339a5f8d8288a53bd679f
nathan bezard <nathan.bezard@epitech.eu>
2025-03-11 18:50:22 +0100

    ✨ Add Unlockable class

---
+++ src/Game/Blocks/Builds/Unlockable.cpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2025
+** FactoryHub
+** File description:
+** Unlockable
+*/
+
+#include "Unlockable.hpp"
+
+Unlockable::Unlockable(/* args */)
+{
+    _unlockedList.push_back("treadmill");
+    _unlockedList.push_back("MinerT1");
+    _unlockedList.push_back("Turret");
+    _unlockedList.push_back("Base");
+}
+
+Unlockable::~Unlockable()
+{
+}
---
+++ src/Game/Blocks/Builds/Unlockable.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2025
+** FactoryHub
+** File description:
+** Unlockable
+*/
+
+#pragma once
+#include <vector>
+#include <string>
+
+class Unlockable
+{
+    public:
+        Unlockable();
+        ~Unlockable();
+    private:
+    std::vector<std::string> _unlockedList;      
+};
+
+
+//tapis
+//mineur t1
+//tourelle
+//base
+//
+//
+//
