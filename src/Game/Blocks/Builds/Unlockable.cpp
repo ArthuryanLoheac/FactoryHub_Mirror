f77b1990e58cce579604c6681ed88f9560f5ed71
nathan bezard <nathan.bezard@epitech.eu>
2025-03-12 14:04:59 +0100

    ✨ Add all the function to unlock a building

--- src/Game/Blocks/Builds/Unlockable.cpp
+++ src/Game/Blocks/Builds/Unlockable.cpp
@@ -24,3 +24,118 @@ Unlockable::Unlockable(/* args */)
 Unlockable::~Unlockable()
 {
 }
+
+void Unlockable::unlock_Advanced_Assembly_Workshop()
+{
+    _unlockedBuild.push_back("Advanced_Assembly_Workshop");
+}
+
+void Unlockable::unlock_Advanced_Smelter()
+{
+    _unlockedBuild.push_back("Advanced_Smelter");
+}
+
+void Unlockable::unlock_Assembly_Workshop()
+{
+    _unlockedBuild.push_back("Assembly_Workshop");
+}
+
+void Unlockable::unlock_Capacitor_Factory()
+{
+    _unlockedBuild.push_back("Capacitor_Factory");
+}
+
+void Unlockable::unlock_Chemical_Plant()
+{
+    _unlockedBuild.push_back("Chemical_Plant");
+}
+
+void Unlockable::unlock_Cutting_Machine()
+{
+    _unlockedBuild.push_back("Cutting_Machine");
+}
+
+void Unlockable::unlock_Foundry()
+{
+    _unlockedBuild.push_back("Foundry");
+}
+
+void Unlockable::unlock_High_Voltage_Generator()
+{
+    _unlockedBuild.push_back("High_Voltage_Generator");
+}
+
+void Unlockable::unlock_Molding_Workshop()
+{
+    _unlockedBuild.push_back("Molding_Workshop");
+}
+
+void Unlockable::unlock_Nuclear_Refinery()
+{
+    _unlockedBuild.push_back("Nuclear_Refinery");
+}
+
+void Unlockable::unlock_Smelter()
+{
+    _unlockedBuild.push_back("Smelter");
+}
+
+void Unlockable::unlock_Wiring_Factory()
+{
+    _unlockedBuild.push_back("Wiring_Factory");
+}
+
+void Unlockable::unlock_Auto_Turret()
+{
+    _unlockedBuild.push_back("Auto_Turret");
+}
+
+void Unlockable::unlock_Pump_Turret()
+{
+    _unlockedBuild.push_back("Pump_Turret");
+}
+
+void Unlockable::unlock_Gatling_Turret()
+{
+    _unlockedBuild.push_back("Gatling_Turret");
+}
+
+void Unlockable::unlock_Explosive_Turret()
+{
+    _unlockedBuild.push_back("Explosive_Turret");
+}
+
+void Unlockable::unlock_Eclipse_Turret()
+{
+    _unlockedBuild.push_back("Eclipse_Turret");
+}
+
+void Unlockable::unlock_Burst_Turret()
+{
+    _unlockedBuild.push_back("Burst_Turret");
+}
+
+void Unlockable::unlock_Sniper_Turret()
+{
+    _unlockedBuild.push_back("Sniper_Turret");
+}
+
+void Unlockable::unlock_Treadmill_T2()
+{
+    _unlockedBuild.push_back("Treadmill_T2");
+}
+
+void Unlockable::unlock_Treadmill_T3()
+{
+    _unlockedBuild.push_back("Treadmill_T3");
+}
+
+void Unlockable::unlock_Miner_T2()
+{
+    _unlockedBuild.push_back("Miner_T2");
+}
+
+void Unlockable::unlock_Miner_T3()
+{
+    _unlockedBuild.push_back("Miner_T3");
+}
--- src/Game/Blocks/Builds/Unlockable.hpp
+++ src/Game/Blocks/Builds/Unlockable.hpp
@@ -15,86 +15,81 @@ class Unlockable
         Unlockable();
         ~Unlockable();
 
+        void unlock_Advanced_Assembly_Workshop();
+        void unlock_Advanced_Smelter();
+        void unlock_Assembly_Workshop();
+        void unlock_Capacitor_Factory();
+        void unlock_Chemical_Plant();
+        void unlock_Cutting_Machine();
+        void unlock_Foundry();
+        void unlock_High_Voltage_Generator();
+        void unlock_Molding_Workshop();
+        void unlock_Nuclear_Refinery();
+        void unlock_Smelter();
+        void unlock_Wiring_Factory();
+
+        void unlock_Auto_Turret();
+        void unlock_Pump_Turret();
+        void unlock_Gatling_Turret();
+        void unlock_Explosive_Turret();
+        void unlock_Eclipse_Turret();
+        void unlock_Burst_Turret();
+        void unlock_Sniper_Turret();
+
+        void unlock_Treadmill_T2();
+        void unlock_Treadmill_T3();
+
+        void unlock_Miner_T2();
+        void unlock_Miner_T3();
+
         static Unlockable *instance;
     private:
     std::vector<std::string> _unlockedBuild;      
 };
 
 
-//tapis = treadmill
-//mineur t1 = MinerT1
-//tourelle = Turret
+////Unlocked at the start
+//
+//tapis = Treadmill_T1
+//mineur t1 = Miner_T1
+//tourelle = Improvised_Turret
 //base = Base
 //
+////FACTORY////
+//
 //Advanced_Assembly_Workshop
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+//Advanced_Smelter
+//Assembly_Workshop
+//Capacitor_Factory
+//Chemical_Plant
+//Cutting_Machine
+//Foundry
+//High_Voltage_Generator
+//Molding_Workshop
+//Nuclear_Refinery
+//Smelter
+//Wiring_Factory
+//
+////TURRET////
+//
+//Improvised_Turret --> Unlocked at the start
+//Auto_Turret
+//Pump_Turret  --> Tourelle à cartouche
+//Gatling_Turret  --> Tourelle à chaine
+//Explosive_Turret
+//Eclipse_Turret
+//Burst_Turret  --> Tourelle à rafale
+//Sniper_Turret
+//
+////TREADMILL (Tapis) ////
+//
+//Treadmill_T1 --> Unlocked at the start
+//Treadmill_T2
+//Treadmill_T3
+//
+////Miner////
+//
+//Miner_T1 --> Unlocked at the start
+//Miner_T2
+//Miner_T3
 //
