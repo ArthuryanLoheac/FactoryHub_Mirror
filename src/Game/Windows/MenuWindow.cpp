62864626844bc3c5ce56fce9c1624b4253eecdb0
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-03-26 17:24:34 +0100

    ✨ Add loading page when generating map

--- src/Game/Windows/MenuWindow.cpp
+++ src/Game/Windows/MenuWindow.cpp
@@ -12,7 +12,10 @@ void WindowsManager::initMenu(MapGrid &map)
 {
     _startMenu = new sdf::UISprite(glm::vec3(0.0f, 0.0f, 80.0f),
         sdf::GetterTextures::instance->getTexture("StartMenu"));
+    _loadingMenu = new sdf::UISprite(glm::vec3(0.0f, 0.0f, 80.0f),
+        sdf::GetterTextures::instance->getTexture("LoadingMenu"));
     _startMenu->setSize(glm::vec2(1080.0f, 720.0f));
+    _loadingMenu->setSize(glm::vec2(1080.0f, 720.0f));
 }
 
 void WindowsManager::drawMenu(MapGrid map, sdf::Renderer &renderer)
@@ -29,6 +32,9 @@ void WindowsManager::processInputsMenu(GLFWwindow *window, sdf::Renderer &render
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
     if (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS){
+        renderer.clear();
+        _loadingMenu->draw(renderer);
+        renderer.swapBuffers();
         _state = State::GAME;
         init(map);    
     }
